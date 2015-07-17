ee35ca60848adc01bd57c1258b3285627820ec97
ninjawhale <hoshrio@gmail.com>
2015-07-17 00:45:18 -0700

    Add Script for Retrieving Teacher Evals

--- retrieveTeacherEvals.py
+++ retrieveTeacherEvals.py
@@ -1,7 +1,9 @@
 # Web Page Scraper for Teacher Evals Page
 # github/mikur/mikurio
 
-import mechanicalsoup, os, argparse
+import mechanicalsoup, os, argparse, string
+from robobrowser import RoboBrowser
+from selenium import webdriver
 
 parser = argparse.ArgumentParser(description='WebLogin to UW')
 parser.add_argument('username')
@@ -9,14 +11,30 @@ parser.add_argument('password')
 
 args = parser.parse_args()
 
+"""
+# using mechanical browser, but need javascript so using selenium.
 browser = mechanicalsoup.Browser()
-
 login_page = browser.get('https://weblogin.washington.edu/')
-login_form = login_page.soup.body.div.div.div.form.ul
-#login_form = login_page.soup.select(".weblogin_netid")
+login_form = login_page.soup.body.div.div.div.form
+#print(login_form)
 
 login_form.select("#weblogin_netid")[0]['value'] = args.username 
 login_form.select("#weblogin_password")[0]['value'] = args.password
-
 logged_page = browser.submit(login_form, login_page.url)
+
+mainPage = browser.get('https://www.washington.edu/cec/a-toc.html')
+"""
+
+"""
+selenium_browser = webdriver.Firefox()
+selenium_browser.get('https://weblogin.washington.edu')
+loginElement = selenium_browser.find_element_by_id('weblogin_netid')
+loginElement.send_keys(args.username)
+passwordElement = selenium_browser.find_element_by_id('weblogin_password')
+passwordElement.send_keys(args.password)
+passwordElement.submit()
+"""
+
+list_of_sites = ['https://www.washington.edu/cec/{0}-toc.html'.format(s) for s in string.ascii_lowercase[:23]]
+		
 
