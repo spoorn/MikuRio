0ac8ec7df152298d80bc0672c75ef59fc245bd03
ninjawhale <hoshrio@gmail.com>
2015-07-18 23:15:12 -0700

    Log in to WebLogin and retrieve all links to courses

--- retrieveTeacherEvals.py
+++ retrieveTeacherEvals.py
@@ -1,40 +1,67 @@
-# Web Page Scraper for Teacher Evals Page
-# github/mikur/mikurio
+"""
+Web Page Scraper for Teacher Evals Page
+github/mikur/mikurio
+"""
 
 import mechanicalsoup, os, argparse, string
 from robobrowser import RoboBrowser
 from selenium import webdriver
 
-parser = argparse.ArgumentParser(description='WebLogin to UW')
-parser.add_argument('username')
-parser.add_argument('password')
+teacher_links =[]
+browser = webdriver.Firefox()
 
-args = parser.parse_args()
+def clean_links(links):
+	"""
+	Remove links that don't link to teacher evals
+	"""
+	return links[10:-3]
 
-"""
-# using mechanical browser, but need javascript so using selenium.
-browser = mechanicalsoup.Browser()
-login_page = browser.get('https://weblogin.washington.edu/')
-login_form = login_page.soup.body.div.div.div.form
-#print(login_form)
+def get_userpass():
+	"""
+	Get Username and Password User entered into Command Line
+	Returns parser
+	"""
+	parser = argparse.ArgumentParser(description='WebLogin to UW')
+	parser.add_argument('username')
+	parser.add_argument('password')
 
-login_form.select("#weblogin_netid")[0]['value'] = args.username 
-login_form.select("#weblogin_password")[0]['value'] = args.password
-logged_page = browser.submit(login_form, login_page.url)
+	return parser.parse_args()
 
-mainPage = browser.get('https://www.washington.edu/cec/a-toc.html')
-"""
+def login(user_input):
+	"""
+	Logs into WebLogin UW
+	Username and Password given work
+	Pass in Selenium browser to be in the same session
+	"""
+	browser.get('https://weblogin.washington.edu')
+	loginElement = browser.find_element_by_id('weblogin_netid')
+	loginElement.send_keys(user_input.username)
 
-"""
-selenium_browser = webdriver.Firefox()
-selenium_browser.get('https://weblogin.washington.edu')
-loginElement = selenium_browser.find_element_by_id('weblogin_netid')
-loginElement.send_keys(args.username)
-passwordElement = selenium_browser.find_element_by_id('weblogin_password')
-passwordElement.send_keys(args.password)
-passwordElement.submit()
-"""
+	passwordElement = browser.find_element_by_id('weblogin_password')
+	passwordElement.send_keys(user_input.password)
+	passwordElement.submit()
 
-list_of_sites = ['https://www.washington.edu/cec/{0}-toc.html'.format(s) for s in string.ascii_lowercase[:23]]
+def get_all_links(url):
+	"""
+	Open a page listing all courses and retrieve all the teacher links
+	"""
+
+	browser.get(url)
+	return clean_links(browser.find_elements_by_tag_name('a'))
+
+def main():
+	
+	login(get_userpass())
+	
+	list_of_sites = ['https://www.washington.edu/cec/{0}-toc.html'.format(s) for s in string.ascii_lowercase[:23]]
+	
+	for url in list_of_sites:
+		teacher_links.extend(get_all_links(url))
+	
+	
+	print('\n\n\n{}'.format(len(teacher_links)))
+	print("Done")	
 		
 
+if __name__ == '__main__':
+	main()
